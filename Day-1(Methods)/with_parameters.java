0708c4e53e8b83c44b25793920fc2e93e77de79b
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-05 23:43:08 +0530

    Update with_parameters.java

--- Day-1(Methods)/with_parameters.java
+++ Day-1(Methods)/with_parameters.java
@@ -1,1 +1,11 @@
+public class SimpleProgram {
+    public static void addNumbers(int num1, int num2) {
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+        addNumbers(number1, number2);     }
+}
 
