782f34e69bc0efa6656b787cf1e1f1a7b92127c8
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-06 07:59:56 +0530

    Static-code.java

--- Static_BLock/Static-code.java
+++ Static_BLock/Static-code.java
@@ -1,1 +1,19 @@
 
+Code:
+public class q2{
+    public static void main(String[] args){
+        Demo d1 = new Demo();
+        Demo d2 = new Demo();
+    }
+}
+//when craeted objects constructor invoked automatically.
+class Demo{
+    static{//syntax for static block-(singleton-block)
+        System.out.println("stb");
+    }
+    Demo(){//constructor->initialization of objectts
+        System.out.println("cns");
+    }
+}
+
+//we created two objects so 2 times the constructor will be called!
