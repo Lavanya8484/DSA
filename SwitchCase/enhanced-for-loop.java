090b2ed646354dcdd66926d42b1385c335474a33
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-08 10:04:02 +0530

    Enhanced for-loop.java

--- SwitchCase/enhanced-for-loop.java
+++ SwitchCase/enhanced-for-loop.java
@@ -1,1 +1,13 @@
-
+For-Each loop / enhanced for loop:-
+In collections we dont have index values in those situations we have to use for-each loop
+->Example:
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      
+      int[] arr = {1,2,3,4,5};
+      for(int var :arr){
+        System.out.println(var);
+    }
+  }
+}
