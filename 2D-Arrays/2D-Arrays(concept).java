598d4dac364b2dcc1cf1e59e203e9b432ea58dc7
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-10 10:37:06 +0530

    2D-Arrays(concept).java

---
+++ 2D-Arrays/2D-Arrays(concept).java
@@ -0,0 +1,8 @@
+2D Array:
+2-dimensional arrays consists of rows and coloumns
+Syntax:
+int[][] arr = new int[3][2]; -> the first square represents "rows" and second square bracket represents "coloumns"
+-> While declaring the array we have to declare number of rows and coloumn numbers are optional 
+-> Each row will act as an individual array 
+arr[5] = {1,2,3} size = 3, length = 5
+for 2-d array the size will be first sq.bracket
