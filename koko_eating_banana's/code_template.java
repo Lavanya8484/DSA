b364c01dc191f4d05616fa38aa54b4d87c7090bf
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-23 09:52:55 +0530

    code_template.java

---
+++ koko_eating_banana's/code_template.java
@@ -0,0 +1,11 @@
+whatever maximum values we have we have to check that values (run the loop from 0 to max)
+i) check the elements till Max
+ii) Initialize left pointer to zero
+iii) Initialize right pointer to Max_value
+iv) Get the middle value and divide the array elements 
+v) get the ceil values 
+vi) Add all those ceil values and compare with target hours
+vii) If it is <= (lessthan or equalto) eliminate right Half 
+viii) Change the right pointer to mid-1
+ix) If the Sum is greater than to the target -> Eliminate left half and change left pointer to mid + 1
+x) return left 
