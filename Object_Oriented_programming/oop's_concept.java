41d5a1e2b9a65194907105447ef176036979e0e6
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-23 10:34:18 +0530

    oop's_concept.java

---
+++ Object_Oriented_programming/oop's_concept.java
@@ -0,0 +1,47 @@
+4 pillars of object oriented programming are :
+  i) Inheritance
+  ii)Polymorphism
+  iii)Encapsulation
+  iV) abstraction
+-> To archive above properties the pre-requriements are Class,Object,Constructor and Interface
+->Class : Class is a Virtual entity and Blueprint of Objects, class is a place where we define objects 
+->Object : Instance of a class is called object , we can call the methods which are present in that particular class by creating objects for that 
+           particular class 
+->Constructor : Initializing the objects 
+Two-Types: i)Default constructor ii) Parametrized constructor
+->i) For every class there will be default default constructors 
+->ii) For every class there will be firstBenchers they will have some parameters(like, attendance,marks....) in similar way the constructors which are having 
+     parameters are known as parametrized constructors. 
+
+Inheritance :- There are 4-types
+  i) Single
+  ii) Multi-Level
+  iii) Multiple
+  iv) Heirarcal Inheritance
+
+Single Inheritance:- The properties are inherited from parent class to child class 
+Multi-Level Inheritance:- Properties are inherited from parent class to GrandParent class and Parent Class
+Multiple :- Properties are inherited from multiple parent classes to child class
+Hierachical :- Properties are inherited to multiple child classes from single Parent class
+->using extend keyword we can inherit the properties from "classes" 
+-> using implement keyword we can inherit the properties from "Interfaces" 
+
+Polymorphism :- Same method Appearing multiple times 
+  Two types:i)method-overloading ii) method-overriding
+  i)Method-overloading: in same class if we have multiple methods with same name with differnt parameters 
+  ii) method-overriding: in child class if we declare a method which were present in parent class 
+     in same class if we have multiple methods with same  names with same parameters
+ NOTE:Main method can be over-ridden with different parameters 
+
+Encapsulation :- binding methods and data and hiding sensitive data is called encapsulation 
+  -> if the variables are private we cant access outside the class, if we want to access we can access using methods
+
+Abstraction :- Hiding implementation details and showing only essesntial  details to the user
+  Four-types : i) data-abstraction ii) Functional-Abstraction iii)Procedural-Abstraction iv)Control-Abstraction
+  ->If we want to make method abstract we have to use "Abstract" keyword and if we have Abstract-method we have to make the
+   corresponding class Abstract class
+  -> abstract method is a incomplete method
+
+  Interface :-It contains All abstract methods
+  -> we cant't create objects for interface 
+  
