3000865a9f3785843d9c4f6251b9e3df63a7a07c
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-05-23 08:41:33 +0530

    code_template.java

---
+++ Max_product_subArray/code_template.java
@@ -0,0 +1,9 @@
+input:- [2,3-6,4]
+op:- 6
+-> Start your loop variable from 1 
+i) in the subArray if we have even positve numbers the product will be Positive 
+ii) If we have odd negative numbers then the product will be Negative 
+iii) In the sub Array if we have atleast one Zero then Start your product variable with one
+iv) If we have nagative values the product of prefix elements will be positive and the suffix of negative elements will be positive 
+v) Initialize the maximum variable Extreme negative 
+  
