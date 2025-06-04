7e5769654ca814f7c58aacf15eeda45a2314a7fe
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-06-04 10:28:07 +0530

    BinarySearchTree.java

---
+++ NonLinearDS/BinarySearchTree.java
@@ -0,0 +1,25 @@
+Binary Search Tree :
+Left < Root < Right 
+adv:
+  -For BST, if you print inOrder Traversal you will get output in Sorted Order
+disadv:
+The tree Might Skewed (all nodes have only one child)
+
+*Balanced-Tree : height(left tree) - height(right tree) = -1/0/1
+
+
+*AVL:
+ AVL tree is self-balenced tree
+ There are 4 types:
+i) Left-Left
+ii) Right-Right 
+iii) Right-Left
+iv) Left-Right
+
+*Segment Tree:
+In an array we have to get Sum of particular range values we will use a simple for loop we will get o/p and time complexity will be O(n)
+  and we want to get the o/p in log(n) so we can use segment tree's to get targeted output in log(n) time complexity
+
+
+  
+  
