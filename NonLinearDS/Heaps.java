affb4d4024d143ccb891a924b5cc2ed979b5c32c
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-06-05 08:28:15 +0530

    Heaps.java

---
+++ NonLinearDS/Heaps.java
@@ -0,0 +1,12 @@
+Heap : Heap Data Structure will store the data in arrays ,it will be stored by trees and the values will be arranged by trees
+  - With the help of complete binary tree the elements are arranged accordingly
+  - With the help of priority queues Heaps got created!
+  - Trees + Arrays = Heaps
+  - As the Data are storing in the array so we can Access the elements in arrays and as well as in Trees
+  - it involves in using Tree DataStructure so it is a Non-Linear DataStructure 
+  - There is a Scope of using Index values
+
+  
+There are Two Type of aproaches:
+i)Bottom to Top 
+ii) Top to Bottom 
