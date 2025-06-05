019f5bfebc554726b639d0b6eb78400576ebdaaf
sri_lavanya_tamatapu <138578739+Lavanya8484@users.noreply.github.com>
2025-06-05 08:48:22 +0530

    Tries.java

---
+++ NonLinearDS/Tries.java
@@ -0,0 +1,9 @@
+Tries:
+- We have to create a node which can store character Array of size 26
+- Initialize boolean flags and Pointers - Node next;
+- The last node need to change the flag to True
+
+Advantage :
+ -To get faster retrivals of strings 
+
+A Trie is a tree-like data structure used to efficiently store and retrieve strings, especially useful for prefix-based searching.
